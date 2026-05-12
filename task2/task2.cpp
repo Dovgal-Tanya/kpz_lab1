b3bf9fec4ce71a8be049a5384dea8b6ad51a4040
Dovgal-Tanya <dovgaltanya07@gmail.com>
2026-05-12 15:49:33 +0300

    Update task2.cpp

--- task2/task2.cpp
+++ task2/task2.cpp
@@ -1,4 +1,4 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <Windows.h>
@@ -14,36 +14,37 @@ typedef struct {
     Node** buckets;
 } HashTable;
 
+// ВИПРАВЛЕННЯ Issue 3 (DRY): Функція для перевірки виділення пам'яті
+void check_allocation(void* ptr, const char* message) {
+    if (!ptr) {
+        printf("Помилка: %s\n", message);
+        exit(1);
+    }
+}
+
 void fill_array(int* arr, int size) {
     for (int i = 0; i < size; i++) {
         arr[i] = rand() % size;
     }
 }
 
-int hash(int key, int size) {
+int hash_func(int key, int size) {
     return key % size;
 }
 
 HashTable* create_table(int size) {
     HashTable* table = (HashTable*)malloc(sizeof(HashTable));
-    if (!table) {
-        printf("Помилка створення хеш-таблиці!\n");
-        return NULL;
-    }
+    check_allocation(table, "створення хеш-таблиці");
 
     table->size = size;
     table->buckets = (Node**)calloc(size, sizeof(Node*));
-    if (!table->buckets) {
-        printf("Помилка виділення пам’яті для бакетів!\n");
-        free(table);
-        return NULL;
-    }
+    check_allocation(table->buckets, "виділення пам’яті для бакетів");
 
     return table;
 }
 
 void insert(HashTable* table, int key) {
-    int idx = hash(key, table->size);
+    int idx = hash_func(key, table->size);
     Node* current = table->buckets[idx];
 
     while (current != NULL) {
@@ -55,10 +56,7 @@ void insert(HashTable* table, int key) {
     }
 
     Node* new_node = (Node*)malloc(sizeof(Node));
-    if (!new_node) {
-        printf("Помилка виділення пам’яті для вузла!\n");
-        return;
-    }
+    check_allocation(new_node, "виділення пам’яті для вузла");
 
     new_node->key = key;
     new_node->count = 1;
@@ -83,24 +81,32 @@ int compare(const void* a, const void* b) {
     return (*(int*)a - *(int*)b);
 }
 
+// ВИПРАВЛЕННЯ Issue 2 (Long Method): винесено логіку заповнення
+void rebuild_array(HashTable* table, int* keys, int count, int* arr) {
+    int index = 0;
+    for (int i = 0; i < count; i++) {
+        int key = keys[i];
+        Node* node = table->buckets[hash_func(key, table->size)];
+        while (node != NULL) {
+            if (node->key == key) {
+                for (int j = 0; j < node->count; j++) {
+                    arr[index++] = key;
+                }
+                break;
+            }
+            node = node->next;
+        }
+    }
+}
+
 void count_sort(int* arr, int size) {
-    fill_array(arr, size);
-
-    clock_t start = clock();
-
     HashTable* table = create_table(size);
-    if (!table) return;
-
     for (int i = 0; i < size; i++) {
         insert(table, arr[i]);
     }
 
     int* keys = (int*)malloc(size * sizeof(int));
-    if (!keys) {
-        printf("Помилка виділення пам’яті для ключів!\n");
-        free_table(table);
-        return;
-    }
+    check_allocation(keys, "пам'ять для ключів");
 
     int count = 0;
     for (int i = 0; i < table->size; i++) {
@@ -112,24 +118,7 @@ void count_sort(int* arr, int size) {
     }
 
     qsort(keys, count, sizeof(int), compare);
-
-    int index = 0;
-    for (int i = 0; i < count; i++) {
-        int key = keys[i];
-        Node* node = table->buckets[hash(key, table->size)];
-        while (node != NULL) {
-            if (node->key == key) {
-                for (int j = 0; j < node->count; j++) {
-                    arr[index++] = key;
-                }
-                break;
-            }
-            node = node->next;
-        }
-    }
-
-    clock_t end = clock();
-    printf("Сортування (%d елементів): %.3lf сек\n", size, (double)(end - start) / CLOCKS_PER_SEC);
+    rebuild_array(table, keys, count, arr);
 
     free(keys);
     free_table(table);
@@ -138,21 +127,21 @@ void count_sort(int* arr, int size) {
 int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-
     srand(time(NULL));
 
     int sizes[] = { 100, 1000, 10000 };
     for (int i = 0; i < 3; i++) {
         int size = sizes[i];
         int* arr = (int*)malloc(size * sizeof(int));
-        if (!arr) {
-            printf("Помилка виділення пам'яті для масиву розміру %d!\n", size);
-            continue;
-        }
+        check_allocation(arr, "масив");
 
+        fill_array(arr, size);
+        clock_t start = clock();
         count_sort(arr, size);
+        clock_t end = clock();
+
+        printf("Сортування (%d елементів): %.3lf сек\n", size, (double)(end - start) / CLOCKS_PER_SEC);
         free(arr);
     }
-
     return 0;
 }
